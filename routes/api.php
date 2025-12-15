0843c79423dd3e847e515b08550def8230649e2a
rajesh9658 <rajeshpadhi9658@gmail.com>
2025-12-15 12:56:23 +0530

    add

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\SiteController;
+use App\Http\Controllers\Backend\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login/submit', [LoginController::class, 'Apilogin']);
+Route::post('/logout', [LoginController::class, 'Apilogout']);
+Route::post('/store-device-events', [SiteController::class, 'apiStoreDevice']);
+Route::put('/update-device-events/{deviceId}', [SiteController::class, 'apiUpdateDevice']);
+Route::get('/device-status', [DashboardController::class, 'apiFetchDeviceStatus']);
+Route::post('/send-if-match', [DashboardController::class, 'apiSendEmailIfMatch']);
+Route::middleware('auth:sanctum')->group(function () {
+    // Route::get('/sites', [SiteController::class, 'apiSites']);
+    // Route::get('/dashboard-data', [DashboardController::class, 'apiIndex'])->name('dashboard');
+
+   
+
+});
+Route::post('/mobile-login', [LoginController::class, 'mobileLogin']);
